085a10af8c7e1f98a21cd1d1e2a5c71df348dbc1
Hamza <79455122+HamzaAhmadTipi@users.noreply.github.com>
2023-09-16 18:15:22 +0100

    changed text to speech to gTTS

--- backend/text_to_speech.py
+++ backend/text_to_speech.py
@@ -1,11 +1,17 @@
-import pyttsx3
-
+from gtts import gTTS
+from playsound import playsound
+import os
 
 def text_to_speech(input_text):
-    engine = pyttsx3.init()
-    engine.say(str(input_text))
-    engine.runAndWait()
+    language = 'en'
 
+    # save tts as mp3 file
+    myobj = gTTS(text=input_text, lang=language, slow=False)
+    myobj.save("reading.mp3")
+    os.system("mpg321 reading.mp3")
+
+    # this reads it out the mp3 file
+    playsound('reading.mp3')
 
 if __name__ == "__main__":
     text_to_speech(" Hello, This is a speech to text.")
